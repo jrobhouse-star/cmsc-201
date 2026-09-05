4dda93becc9e65a32b46e1be92e73f55eff84f05
Jonathan House <jhouse3@umbc.edu>
2026-09-04 21:24:44 -0400

    removed input string

--- distance_formula.py
+++ distance_formula.py
@@ -1,6 +1,6 @@
-x1 = float(input("give x1: "))
-y1 = float(input("give y1: "))
-x2 = float(input("give x2: "))
-y2 = float(input("give y2: "))
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
 distance = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 print(f"the distance between ({y1}, {y2}) and ({x1}, {x2}) is {distance}")
