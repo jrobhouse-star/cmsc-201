cb88e719b3c1fc99ec3a0501f53e746962dce70d
Jonathan House <jhouse3@umbc.edu>
2026-09-04 21:23:06 -0400

    session this done

---
+++ distance_formula.py
@@ -0,0 +1,6 @@
+x1 = float(input("give x1: "))
+y1 = float(input("give y1: "))
+x2 = float(input("give x2: "))
+y2 = float(input("give y2: "))
+distance = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(f"the distance between ({y1}, {y2}) and ({x1}, {x2}) is {distance}")
